6017d4072193458457f35e4eede022688388db5a
Anoop Naravaram <anoop901@gmail.com>
2023-12-18 22:23:42 -0800

    day 18 template

--- src/main/kotlin/com/anoopnaravaram/adventofcode/year2023/Day18.kt
+++ src/main/kotlin/com/anoopnaravaram/adventofcode/year2023/Day18.kt
@@ -1,23 +1,23 @@
-package com.anoopnaravaram.adventofcode
+package com.anoopnaravaram.adventofcode.year2023
 
 import com.anoopnaravaram.adventofcode.PuzzleSolution
 
-class DayXX : PuzzleSolution(
-    inputFilePath = "input/2023/dayXX/input.txt",
+class Day18 : PuzzleSolution(
+    inputFilePath = "input/2023/day18/input.txt",
     exampleInput = """
     """.trimIndent(),
 ) {
     override fun part1(): Number {
-        return 0
+        TODO("Not yet implemented")
     }
 
     override fun part2(): Number {
-        return 0
+        TODO("Not yet implemented")
     }
 }
 
 fun main() {
-    val solution = DayXX()
+    val solution = Day18()
     println("part 1: ${solution.part1()}")
     println("part 2: ${solution.part2()}")
 }
