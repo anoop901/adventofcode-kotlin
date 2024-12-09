c57b2f19edec6909e3a266b9f459428851a53495
Anoop Naravaram <anoop901@gmail.com>
2024-12-08 20:56:56 -0800

    optimize 2024 day 7

--- src/main/kotlin/com/anoopnaravaram/adventofcode/year2024/Day7.kt
+++ src/main/kotlin/com/anoopnaravaram/adventofcode/year2024/Day7.kt
@@ -1,6 +1,7 @@
 package com.anoopnaravaram.adventofcode.year2024
 
 import com.anoopnaravaram.adventofcode.PuzzleSolution
+import kotlin.math.sign
 
 private fun <T> cartesianProduct(vararg sets: Set<T>): Set<List<T>> {
     return sets.fold(listOf(listOf<T>())) { acc, set ->
@@ -38,11 +39,24 @@ class Day7 : PuzzleSolution(
             }
         }
 
-        fun canPossiblyBeTrue(availableOperators: Set<String>): Boolean {
-            val possibleOperatorCombos = cartesianPower(availableOperators, operands.size - 1)
-            return possibleOperatorCombos.any { operatorCombo ->
-                evaluate(operatorCombo) == target
+        fun canPossiblyBeTrue(enableConcatenation: Boolean = false): Boolean {
+            if (operands.size == 1) {
+                return target == operands.first()
             }
+            val subEquations = sequence {
+                if (target - operands.last() > 0) {
+                    yield(Equation(target - operands.last(), operands.dropLast(1)))
+                }
+                if (target % operands.last() == 0L) {
+                    yield(Equation(target / operands.last(), operands.dropLast(1)))
+                }
+                if (enableConcatenation && target.toString().endsWith(operands.last().toString())) {
+                    val newTargetStr = target.toString().dropLast(operands.last().toString().length)
+                    val newTarget = if (newTargetStr.isEmpty()) 0 else newTargetStr.toLong()
+                    yield(Equation(newTarget, operands.dropLast(1)))
+                }
+            }
+            return subEquations.any { it.canPossiblyBeTrue(enableConcatenation) }
         }
     }
 
@@ -54,11 +68,11 @@ class Day7 : PuzzleSolution(
     }
 
     override fun part1(): Number {
-        return equations.filter { it.canPossiblyBeTrue(setOf("+", "*")) }.sumOf { it.target }
+        return equations.filter { it.canPossiblyBeTrue() }.sumOf { it.target }
     }
 
     override fun part2(): Number {
-        return equations.filter { it.canPossiblyBeTrue(setOf("+", "*", "||")) }.sumOf { it.target }
+        return equations.filter { it.canPossiblyBeTrue(true) }.sumOf { it.target }
     }
 }
 
